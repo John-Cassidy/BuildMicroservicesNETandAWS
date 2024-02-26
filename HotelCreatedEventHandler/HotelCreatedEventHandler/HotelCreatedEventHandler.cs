f9e2ec0f822168e647ee1cdb9bb15993ecda387c
John Cassidy <jpcassidy@outlook.com>
2024-02-26 15:48:07 -0500

    Create HotelCreatedEventHandler that subscribes to SNS createdHotelEvent

---
+++ HotelCreatedEventHandler/HotelCreatedEventHandler/HotelCreatedEventHandler.cs
@@ -0,0 +1,69 @@
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using Amazon.Lambda.SNSEvents;
+using System.Text.Json;
+using Amazon.DynamoDBv2.DocumentModel;
+using HotelCreatedEventHandler.Models;
+using Microsoft.Extensions.Configuration;
+using Nest;
+
+[assembly: LambdaSerializer(typeof(DefaultLambdaJsonSerializer))]
+
+namespace HotelCreatedEventHandler;
+
+public class HotelCreatedEventHandler
+{
+    private readonly IConfigurationRoot _configurations;
+
+    public HotelCreatedEventHandler()
+    {
+        _configurations = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();        
+    }
+
+    public async Task FunctionHandler(SNSEvent snsEvent, ILambdaContext context)
+    {
+        var dbClient = new AmazonDynamoDBClient();
+        var table = Table.LoadTable(dbClient, "hotel-created-event-ids");
+
+        string domainEndpoint = _configurations["AppSettings:ElasticSearch:DomainEndpoint"];
+        string username = _configurations["AppSettings:ElasticSearch:Username"];
+        string password = _configurations["AppSettings:ElasticSearch:Password"];
+        string indexName = _configurations["AppSettings:ElasticSearch:IndexName"];
+
+        var connectionSettings = new ConnectionSettings(new Uri(domainEndpoint))
+            .BasicAuthentication(username, password)
+            .DefaultIndex(indexName)
+            .DefaultMappingFor<Hotel>(m => m.IdProperty(p => p.Id));
+
+        var esClient = new ElasticClient(connectionSettings);
+
+        if (!(await esClient.Indices.ExistsAsync(indexName)).Exists) {
+            await esClient.Indices.CreateAsync(indexName);
+        }
+
+        foreach (var eventRecord in snsEvent.Records) {
+            
+            var eventId = eventRecord.Sns.MessageId;
+            var foundItem = await table.GetItemAsync(eventId);
+            if ( foundItem == null)
+            {
+                await table.PutItemAsync(new Document{
+                    ["eventId"] = eventId
+                });
+            }
+
+            var hotel = JsonSerializer.Deserialize<Hotel>(eventRecord.Sns.Message);
+            await esClient.IndexDocumentAsync<Hotel>(hotel!);
+
+            context.Logger.LogInformation($"Processed record {eventRecord.Sns.Message}");
+        }
+
+        await Task.CompletedTask;
+    }
+}
